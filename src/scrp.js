d7a5a09b3ad53c70b7e346cc3cfa9edd0b09acbb
AndyDav6 <138633572+AndyDav6@users.noreply.github.com>
2024-07-13 12:44:51 -0500

    HIA V1.113

--- src/scrp.js
+++ src/scrp.js
@@ -14,3 +14,25 @@ function redireccionar () {
 
     window.location.href = link;
 }
+
+
+var modal = document.getElementById("myModal");
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function acceptPrivacy() {
+  modal.style.display = "none";
+}
